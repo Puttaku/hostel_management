0071a022b895a26d9b81bb9c51ef5881ffee8d7b
PUTTIPONG SINTOPPUN <60010742@kmitl.ac.th>
2020-03-13 17:57:55 +0700

    Changelog #6 : Added Roboto font, draft login cardbox

--- src/Component/login.jsx
+++ src/Component/login.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import '../Style/Login.css'
+import '../Style/card.css'
+import '../Style/text.css'
 export default class login extends Component {
     render() {
         return (
             <div>
-                
+                <div className="headingContainer">
+                    <p className="heading login-heading">Hostel</p>
+                </div>
+                <div className = "card login-card">
+                    <div>
+                        
+                    </div>
+                </div>
             </div>
         )
     }
