82e99e9a2eaf1a7fb2ff77a334bc5f9798a9e3c6
PUTTIPONG SINTOPPUN <60010742@kmitl.ac.th>
2020-03-13 16:48:56 +0700

    Changelog #3 : Add login page and Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import {Route} from "react-router-dom"
+import login from "./Component/login"
 function App() {
   return (
     <div className="App">
-      Hello
+      <Route path="/login" component={login}/>
     </div>
   );
 }
