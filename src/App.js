40a0936886456219df9133ce98261011320a4457
PUTTIPONG SINTOPPUN <60010742@kmitl.ac.th>
2020-03-14 20:47:05 +0700

    Changelog #13 : Added Navbar , link complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import './Style/App.css';
+import './Style/navbar.css'
 import {Route} from "react-router-dom"
 import login from "./Component/login"
 import forget from "./Component/forget"
 import redirect from "./Component/redirect"
 import regis from "./Component/regis"
 import home from "./Component/home"
+import Navbar from "./Component/navbar"
+import {withRouter} from "react-router-dom"
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App flexOn">
+        {this.props.location.pathname !== "/login" && this.props.location.pathname !== "/forget" && this.props.history.pathname !== "/register" ? <Navbar/> : ""}
         <Route exact path="/" component={redirect}/>
         <Route exact path="/login" component={login}/>
         <Route exact path="/forget" component={forget}/>
@@ -19,4 +23,4 @@ class App extends Component {
     )
   }
 }
-export default App;
+export default withRouter(App);
