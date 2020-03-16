40f58876116a845fc4556503c7f90a21662a9cc8
PUTTIPONG SINTOPPUN <60010742@kmitl.ac.th>
2020-03-17 01:18:37 +0700

    Changelog #27 : Delete Booked API completed

--- src/Component/book.jsx
+++ src/Component/book.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import "../Style/book.css"
-import {getList} from "../Constant/api"
+import {getList,deleteList} from "../Constant/api"
 export default class book extends Component {
     constructor(){
         super();
         this.state = {
-            list : {}
+            list : []
         }
+        this.deleteBook = this.deleteBook.bind(this);
+        this.addBook = this.addBook.bind(this);
     }
     async componentDidMount(){
+        this.addBook();
+    }
+    async addBook(){
         await fetch(getList,{
             method : "GET",
             header : {
@@ -25,6 +30,18 @@ export default class book extends Component {
             console.log(this.state)
         })
     }
+    async deleteBook(id){
+        await fetch(deleteList+"?username="+localStorage.getItem('username')+"&id="+id,{
+            method : "DELETE",
+            header : {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+        }).then(() =>{
+            console.log("deleted")
+            this.addBook()
+        })
+    }
     render() {
         return (
             <div className="bookedContainer">
@@ -38,20 +55,18 @@ export default class book extends Component {
                             <th className="heading-dou heading2 blackText thaiFont-thick">วันที่ใช้โรงแรม</th>
                             <th className="heading-delete"></th>
                         </tr>
-                        <tr className = "content-table">
-                            <td className="content-name heading2 blackText">Hotel A</td>
-                            <td className="content-price heading2 blackText">9500.-</td>
-                            <td className="content-dob heading2 blackText">19/11/2563</td>
-                            <td className="content-dou heading2 blackText">21/11/2563</td>
-                            <td className="content-delete heading2 blackText">X</td>
-                        </tr>
-                        <tr className = "content-table">
-                            <td className="content-name heading2 blackText">Hotel A</td>
-                            <td className="content-price heading2 blackText">9500.-</td>
-                            <td className="content-dob heading2 blackText">19/11/2563</td>
-                            <td className="content-dou heading2 blackText">21/11/2563</td>
-                            <td className="content-delete heading2 blackText">X</td>
-                        </tr>
+                        {
+                            this.state.list.map((data,index)=>{
+                                return (
+                                    <tr className = "content-table">
+                                        <td className="content-name heading2 blackText">{data.hotelName}</td>
+                                        <td className="content-price heading2 blackText">{data.price}</td>
+                                        <td className="content-dob heading2 blackText">{data.dob}</td>
+                                        <td className="content-dou heading2 blackText">{data.dou}</td>
+                                        <td className="content-delete heading2 blackText" onClick={()=>{this.deleteBook(data.id,index)}}>X</td>
+                                    </tr>
+                            )})
+                        }
                     </table>
                 </div>
             </div>
