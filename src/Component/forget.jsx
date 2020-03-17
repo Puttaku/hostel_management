ea45cc549542547efaa9931e114b9ba14456bfdc
PUTTIPONG SINTOPPUN <60010742@kmitl.ac.th>
2020-03-17 13:36:12 +0700

    Changelog #31 : Added postmanAPI in forget page

--- src/Component/forget.jsx
+++ src/Component/forget.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
 import '../Style/forget.css'
 import '../Style/text.css'
+import {forgetemail} from "../Constant/api"
 export default class forget extends Component {
+    constructor(){
+        super()
+        this.sendEmail = this.sendEmail.bind(this);
+        this.state = {
+            email : false
+        }
+    }
     componentDidMount(){
         document.body.className = "forgetPage";
+    }
+    async sendEmail(){
+        if(this.email.value === ""){
+            alert("กรุณากรอกข้อมูลให้ครบถ้วน")
+        }
+        else{
+            await fetch(forgetemail+"?email="+this.email.value,{
+                method : "PUT",
+                header : {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+            }).then(res => {
+                return res.json()
+            }).then(res => {
+                console.log(res)
+                if(res.Status === 200){
+                    this.setState({
+                        email : true
+                    })
+                }
+                else{
+                    alert(res.Key)
+                }
+            })
+        }
     }
     render() {
         return (
@@ -15,8 +49,9 @@ export default class forget extends Component {
                 </div>
                 <div className="forgetTextbox">
                     <p className="heading2 leftText thaiFont marginRight greyText2">อีเมลล์</p>
-                    <input className="forgetEmail inputBox"/>
-                    <button className="Button forgetButton thaiFont">ตกลง</button>
+                    <input name="email" ref={(e)=>this.email = e} className="forgetEmail inputBox"/>
+                    <button onClick={()=>{this.sendEmail()}} className="Button forgetButton thaiFont">ตกลง</button>
+                    {this.state.email ? <p className="heading2 leftText thaiFont marginRight">link เปลี่ยนรหัสผ่านได้ส่งไปที่อีเมลล์ของคุณแล้ว โปรดทำการตรวจสอบ</p> : ""}
                 </div>
                 <a href="/login" className="content blueText leftText thaiFont">กลับสู่หน้าลงชื่อเข้าใช้</a>
             </div>
